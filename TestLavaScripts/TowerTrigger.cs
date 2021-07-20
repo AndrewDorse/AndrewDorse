e6417ea629b98a9ae438e7d98ca3f5fa8b281e83
Andrew Lisitsyn <55139966+AndrewDorse@users.noreply.github.com>
2021-07-20 16:58:25 +0800

    Update TowerTrigger.cs

--- TestLavaScripts/TowerTrigger.cs
+++ TestLavaScripts/TowerTrigger.cs
@@ -4,25 +4,15 @@ using UnityEngine;
 
 public class TowerTrigger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
-            Debug.Log("asdsffdsdsdsaf");
+          
             other.GetComponent<PlayerController>().TowerPosition();
         }
-        Debug.Log("23u6543567");
+        
     }
 }
